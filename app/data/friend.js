c5e98fefcffad859e1f7456c97baf1a968e2eca2
Robert Bottomley <rbottomley2@att.net>
2017-11-04 03:32:17 -0700

    new commit

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with my data
+var friendList = [
+  {
+    name: 'Robert Bottomley',
+    profilePic: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7b/7b130f6e41319a5239e9912de94eede5e37332fa_full.jpg',
+    scores: [
+      "5",
+      "5",
+      "3",
+      "1",
+      "2",
+      "4",
+      "2",
+      "3",
+      "5",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
